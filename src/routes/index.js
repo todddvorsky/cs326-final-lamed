3ec2e239031f6e577c0e27d7c359453ddb65d951
pkelley98 <44107567+pkelley98@users.noreply.github.com>
2020-11-06 00:46:42 -0500

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,41 @@
-/*var router = require('express').Router();
-
-router.use('/api', require('./api'));
-
-module.exports = router;*/
+'use strict';
 const express = require('express');
 const app = express();
-const dietFuncs = require('./api/diets.js');
-app.use(express.static('./src/views'));
-dietFuncs.d;
-app.listen(8080);
+app.use(express.json()); // lets you handle JSON input
+const port = 8080;
+let datastore = {};
 
-//dietFuncs.d
+app.use('/', express.static('./src/views'));
+
+//   curl -d '{ "value" : "12" }' -H "Content-Type: application/json" http://localhost:3000/read/x
+app.get('/create', (req, res) => {
+    const k = req.query.key;
+    const v = req.query.value;
+    datastore[k] = v;
+    console.log(`Set ${k} to ${v}`);
+    res.send('Set.');
+});
+app.get('/read', (req, res) => {
+    const k = req.query.key;
+    const v = datastore[k];
+    res.send(`key = ${k}, value = ${v}`);
+});
+app.get('/read/:key', (req, res) => {
+    const k = req.params['key'];
+    const v = datastore[k];
+    res.send(`key = ${k}, value = ${v}`);
+});
+//   curl -d '{ "key" : "x", "value" : "12" }' -H "Content-Type: application/json" http://localhost:3000/pcreate
+app.post('/pcreate', (req, res) => {
+    const k = req.body["key"];
+    const v = req.body["value"];
+    datastore[k] = v;
+    console.log(`Set ${k} to ${v}, body = ${JSON.stringify(req.body)}`);
+    res.send('Set.');
+});
+app.get('*', (req, res) => {
+    res.send('NO FOOL');
+});
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
