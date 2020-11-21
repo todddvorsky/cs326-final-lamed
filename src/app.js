dffa5c4a410b13db404d4fc9f6f1b0ab1f27c85a
Jason Bolton <boltydawg@gmail.com>
2020-11-21 15:16:14 -0500

    define index in express

--- src/app.js
+++ src/app.js
@@ -10,9 +10,13 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {index: "home.html"}));
 
 app.use(require('./routes'));
+
+app.get('/', checkLoggedIn, (req, res) => {
+	res.render('diets', { title: 'Express' });
+});
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
