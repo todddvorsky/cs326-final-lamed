8e8df1ed07843cd2341b9e61857bd2fefa77e16e
Jason Bolton <boltydawg@gmail.com>
2020-11-22 04:12:50 -0500

    remove render call that was accidentally re-added during a merge

--- src/app.js
+++ src/app.js
@@ -198,7 +198,7 @@ app.use(function (err, req, res, next) {
 
 	// render the error page
 	res.status(err.status || 500);
-	res.render('error');
+	res.json({error: err});
 });
 
 app.listen(process.env.PORT || 8080);
