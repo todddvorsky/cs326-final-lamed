27495cb8048f35965327189acf98d0670e293c16
Jason Bolton <boltydawg@gmail.com>
2020-11-22 03:46:35 -0500

    remove dead code

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,6 @@
 'use strict';
 
 const router = require('express').Router();
-/* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
-  });
 
 /* use next middleware functions */
 router.use('/users', require('./users'));
