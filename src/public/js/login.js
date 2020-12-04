2e70fabf726fdb8ff2449e68ac3e50e157eacd5d
Jason Bolton <boltydawg@gmail.com>
2020-12-03 21:32:51 -0500

    add alert for failed login attempts

--- src/public/js/login.js
+++ src/public/js/login.js
@@ -20,6 +20,9 @@ window.addEventListener("load", () =>{
             const html = await res.text();
             document.body.innerHTML = html;
         }
+        else{
+            alert('Failed to login');
+        }
     });
     
     document.getElementById('register-form').addEventListener('submit', async function(event) {
